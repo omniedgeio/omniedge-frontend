847cb60979b8d645d4532f3bd9bef1b587cdd814
Tex Tang <tex_tang@outlook.com>
2021-07-18 12:22:55 +0800

    Remove unused font weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,12 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&display=swap"
             rel="stylesheet"
           />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />

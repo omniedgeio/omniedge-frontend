6e7ea7b774ada7d160a7ade0f1b50401516847e4
brucebot <bruceboty@gmail.com>
2022-01-24 01:29:11 +0800

    integrated google analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,6 +38,21 @@ function App({ Component, pageProps }: AppWithLayoutProps) {
                     script-src-elem 'self';
                     style-src-elem 'self';
 "></meta> */}
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KS4W90QM7H"></script>
+<script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KS4W90QM7H', {
+                page_path: window.location.pathname,
+              });
+          `
+            }}
+          />
+
 </Head>
         <Layout>
           <Component {...pageProps}></Component>

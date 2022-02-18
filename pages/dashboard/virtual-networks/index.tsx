31a3ad6bc30a0dcff5157295abad588521ab496b
Yong <hi@omniedge.io>
2022-02-18 13:26:33 +0800

    add upgrade plan to virutal network page

--- pages/dashboard/virtual-networks/index.tsx
+++ pages/dashboard/virtual-networks/index.tsx
@@ -1,10 +1,19 @@
-import { Button, Heading, HStack, VStack } from "@chakra-ui/react";
+import { Button, Heading, HStack, VStack,Alert,AlertIcon } from "@chakra-ui/react";
 import DashboardLayout from "../../../components/layout/Dashboard";
 import Link from "../../../components/next/Link";
 import VirtualNetworksTable from "../../../components/virtual-networks/Table";
 import { Page } from "../../../types";
-
+import { PlanProps, PLANS } from "../../../components/Plans";
+import { useUser } from "../../../lib/hook/useUser";
+import { createPortalSession } from "../../../lib/api/billing";
+import { showError } from "../../../lib/helpers/toast";
 const VirtualNetworkPage: Page = function (props) {
+  const { user, isLoading } = useUser("/login");
+
+  console.log(user);
+  const plan: PlanProps = user?.subscription ? PLANS[user?.subscription.slug as string] : null;
+
+
   return (
     <VStack w="full" alignItems="flex-start" spacing="4">
       <HStack w="full" justifyContent="space-between">
@@ -17,8 +26,25 @@ const VirtualNetworkPage: Page = function (props) {
           </Button>
         </Link>
       </HStack>
+      {user?.subscription.slug !== "free" ? "Upgrade Plan" :
+      <Alert
+       status='info'
+       alignItems='center'
+       justifyContent='center'
+       textAlign='center'
+       >
+    <AlertIcon />
+        Need more than one Virtual Network ? ➡
+          <Link href="/dashboard/billing/choose-plan" color="brand.700">
+              Upgrade Plan
+            </Link>
+  </Alert>}
       <VirtualNetworksTable />
+  
+
+      
     </VStack>
+    
   );
 };
 
